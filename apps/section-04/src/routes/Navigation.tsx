6a375b0853ea440bbd223efcd3c49aa80d5c8dc1
Francisco Marcano <fjmn2001@gmail.com>
2022-11-14 18:20:10 -0400

    adding dinamycs routes. Ref sn

--- apps/section-04/src/routes/Navigation.tsx
+++ apps/section-04/src/routes/Navigation.tsx
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 
 import logo from "./../../public/vite.svg";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { routes } from "./routes";
 
 export const Navigation = () => {
   return (
@@ -16,37 +16,27 @@ export const Navigation = () => {
         <nav>
           <img src={logo} alt="logo" />
           <ul>
-            <li>
-              <NavLink
-                to={"/lazy1"}
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                Lazy 1
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/lazy2"}
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                Lazy 2
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={"/lazy3"}
-                className={({ isActive }) => (isActive ? "nav-active" : "")}
-              >
-                Lazy 3
-              </NavLink>
-            </li>
+            {routes.map(({ name, to }) => (
+              <li key={to}>
+                <NavLink
+                  to={to}
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
 
         <Routes>
-          <Route path={"/lazy1"} element={<LazyPage1 />} />
-          <Route path={"/lazy2"} element={<LazyPage2 />} />
-          <Route path={"/lazy3"} element={<LazyPage3 />} />
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.Component()}
+            />
+          ))}
           <Route path={"/*"} element={<Navigate to={"/lazy1"} replace />} />
         </Routes>
       </div>
