c5e40bbdec21242fd17295f1cb0506d660cffe67
Francisco Marcano <fjmn2001@gmail.com>
2022-12-01 21:03:57 -0400

    show items of shopping cart. Ref sn

--- apps/section-07/src/02-component-patterns/pages/ShoppingPage.tsx
+++ apps/section-07/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -80,26 +80,20 @@ export const ShoppingPage = () => {
       </div>
 
       <div className={"shopping-cart"}>
-        <ProductCard
-          product={product2}
-          className={"bg-dark"}
-          style={{ width: "100px" }}
-        >
-          <ProductImage className={"custom-image"} />
-          <ProductButtons className={"custom-buttons"} />
-        </ProductCard>
-        <ProductCard
-          product={product1}
-          className={"bg-dark"}
-          style={{ width: "100px" }}
-        >
-          <ProductImage className={"custom-image"} />
-          <ProductButtons className={"custom-buttons"} />
-        </ProductCard>
-      </div>
-
-      <div>
-        <code>{JSON.stringify(shoppingCart, null, 5)}</code>
+        {Object.entries(shoppingCart).map(([, product]) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className={"bg-dark"}
+            style={{ width: "100px" }}
+          >
+            <ProductImage className={"custom-image"} />
+            <ProductButtons
+              className={"custom-buttons"}
+              style={{ display: "flex", justifyContent: "center" }}
+            />
+          </ProductCard>
+        ))}
       </div>
     </div>
   );
